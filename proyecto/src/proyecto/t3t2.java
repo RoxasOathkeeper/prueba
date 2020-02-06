f5ec1e375e79c09f18990b760cc59267c211fbff
RoxasOathkeeper <andresmocholiiglesias@gmail.com>
2020-02-06 12:27:56 +0100

    Nueva versión
    He añadido funcionalidad
    
    Signed-off-by: RoxasOathkeeper <andresmocholiiglesias@gmail.com>

--- proyecto/src/proyecto/t3t2.java
+++ proyecto/src/proyecto/t3t2.java
@@ -3,8 +3,11 @@ package proyecto;
 public class t3t2 {
 
 	public static void main(String[] args) {
-		// TODO Apéndice de método generado automáticamente
-
+		System.out.println("ZA WARUDO");
+		System.out.println("La sociedad succiona el almo de un humano. Es de esta manera que volveré a JoJo un cobarde. Entonces podré tomar todo, ¡cuando crezcamos todo será mio!");
+		System.out.println("Si algo he aprendido en mi corta vida, es que mientras más planes haces, más posibilidades hay de que fallen.");
+		System.out.println("No fue JoJo quien te robó tu primer beso. ¡Fui yo! ¡DIO!");
 	}
+
 
 }
